83704e2e3bed1095c33f31e0275335ea1e0d67db
Jitender Singh <jitender@togedr.com>
2020-08-19 18:19:26 +0530

    Add dummy tasks object

--- to-do-list/app.js
+++ to-do-list/app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+let tasks = [
+	{
+		'title' : 'Update title tag',
+		'description' : 'Update title tag of all pages of website'
+	}
+]
 app.use(express.static('public'));
 
 app.listen(port);
