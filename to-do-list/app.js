3b9bb61bbffd8a3636e58494a6fa77afcc409e3d
Jitender Singh <jitender@togedr.com>
2020-08-19 19:05:12 +0530

    Include body parser

--- to-do-list/app.js
+++ to-do-list/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
 
 let tasks = [
 ];
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(function(req, res, next){
 	console.log(`request url ${req.url} \n request method ${req.method}\n`);
