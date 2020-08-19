5e0fd7a2bb07b8d3aaae2423da7b53c1d4282feb
Jitender Singh <jitender@togedr.com>
2020-08-19 18:49:01 +0530

    Add tasks get url

--- to-do-list/app.js
+++ to-do-list/app.js
@@ -3,11 +3,11 @@ const app = express();
 const port = 3000;
 
 let tasks = [
-	{
-		'title' : 'Update title tag',
-		'description' : 'Update title tag of all pages of website'
-	}
-]
+];
 app.use(express.static('public'));
+
+app.get('/tasks', function(req, res){
+	res.json(tasks);
+})
 
 app.listen(port);
