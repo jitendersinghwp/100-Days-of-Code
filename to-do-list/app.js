03827f7c3480cbc83a12ad264b8d5090c536f979
Jitender Singh <jitender@togedr.com>
2020-08-19 18:58:52 +0530

    Include cors module

--- to-do-list/app.js
+++ to-do-list/app.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
+
 let tasks = [
 ];
-app.use(express.static('public'));
 
+app.use(function(req, res, next){
+	console.log(`request url ${req.url} \n request method ${req.method}\n`);
+	console.log(req.body);
+	next();
+});
+
+app.use(express.static('public'));
+app.use(cors());
 app.get('/tasks', function(req, res){
 	res.json(tasks);
 })
